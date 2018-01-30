e20bc90913498ea308598479fc300623a08a64b3
LLC <d40614d@gmail.com>
2018-01-30 23:21:51 +0800

    Update get_offer_api.py
    加入註解

--- Demo3/OFFER/get_offer_api.py
+++ Demo3/OFFER/get_offer_api.py
@@ -16,7 +16,6 @@ import re
 import sort_offer_function
 
 ip = 'redis'
-#tagging = 'tagserver'
 tagging = 'llc_get_tag_value_api'
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -25,28 +24,25 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 r1 = redis.Redis(host=ip, port=6379, db=1)
 
-@app.route('/GetOfferList',methods=['GET'])
+
+@app.route('/GetOfferList',methods=['GET']) #API名稱為GetOfferList，運用Get的方式
 def get_offerlist():
-	vid = str(request.args.get('vid'))
-	tagid = str(request.args.get('tagid'))
-	url = "http://%s/GetTagValue"%tagging
+	vid = str(request.args.get('vid')) #需要取得虛擬ID和TagID兩個變數
+	tagid = str(request.args.get('tagid')) 
+	url = "http://%s/GetTagValue"%tagging #呼叫另一支GetTagValue的API
 	data = {'vid':vid, 'tagid':tagid}
 	tag_info = requests.get(url, params=data)	
-	#tag_info = json.loads(tag_info.content)
-	tag_value = int(tag_info.content)
-	#print r.get(tag_info.conent)
-	offer_list = r1.get(tag_value)
+	tag_value = int(tag_info.content) #取到客群值的格式為str，將其轉為int
+	offer_list = r1.get(tag_value) #呼叫redis DB並取得推薦內容
 	
-	tmp = offer_list.replace("'",'"')
+	tmp = offer_list.replace("'",'"') #offer_list資料格式轉換
 	test = re.sub(ur'u"','"',tmp)
-	offer_dict = json.loads(test)
-	offer_list = offer_dict['offer_list']
-	#offer_list = jsonify(offer_list)
-	#return offer_list
+	offer_dict = json.loads(test) #將其轉為json格式
+	offer_list = offer_dict['offer_list'] #並取得offer_list這個key所對應的value
 	
 	# Sort offer according to offer score 
 
-	recommend_offer_list = sort_offer_function.get_recommend_offer(vid, tagid, offer_list)
+	recommend_offer_list = sort_offer_function.get_recommend_offer(vid, tagid, offer_list) #將推薦內容透過演算法進行排序
 	recommend_dict = {'offer_list':recommend_offer_list}
 	sorted_offer_list = jsonify(recommend_dict)
 
