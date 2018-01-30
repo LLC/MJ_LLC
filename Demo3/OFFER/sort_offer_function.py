174eb54b19d5fb40c2369e3e715fc4ef92596fe0
LLC <d40614d@gmail.com>
2018-01-30 23:49:46 +0800

    加入部分註解

--- Demo3/OFFER/sort_offer_function.py
+++ Demo3/OFFER/sort_offer_function.py
@@ -7,22 +7,22 @@ ip = 'redis'
 r0 = redis.Redis(host=ip, port=6379, db=0)
 
 xls = pd.ExcelFile('OfferTag_1120.xlsx')
-offer_label_table = xls.parse('OFFER ID')
+offer_label_table = xls.parse('OFFER ID') #讀取excel中OFFER ID這個資料表
 
-offer_label_tag = list(offer_label_table)[3:-1]
+offer_label_tag = list(offer_label_table)[3:-1] #取得欄位名稱LABEL1~LABEL18(最後一個LABEL)
 offer_label_tag
 
 # All tags of each offer id
 offer_label_table['offer_label_list'] = ""
 offer_label_Json = []
 
-for i in range(offer_label_table.shape[0]):
+for i in range(offer_label_table.shape[0]): 
     temp_list = []
     temp_detail = {}
-    temp_detail['offer_id'] = str(offer_label_table.loc[i,:]['OFFER_ID'])
+    temp_detail['offer_id'] = str(offer_label_table.loc[i,:]['OFFER_ID']) #將每一個OFFER ID存到temp_detail這個dictionary之中
     
     for column in offer_label_tag:
-        if not pd.isnull(offer_label_table.loc[i,:][column]):
+        if not pd.isnull(offer_label_table.loc[i,:][column]): #將每一個OFFER的所有LABEL都存入temp_list之中
             temp_list.append(offer_label_table.loc[i,:][column])
         else:
             break
@@ -55,10 +55,6 @@ def get_recommend_offer(vid, tagid, lst):
 # All tags of one vid
 def usr_tags(vid, tagid):
 	usr_tags = []
-	#for i in range(len(json.loads(r0.get(vid))['batchTag'])):
-	#    for j in range(len(json.loads(r0.get(vid))['batchTag'][i]['tagName'])):
-	#        usr_tags.append(json.loads(r0.get(vid))['batchTag'][i]['tagName'][j])
-	
 	for i in range(len(json.loads(r0.get(vid))['batchTag'])):
 	    if str(json.loads(r0.get(vid))['batchTag'][i]['tagId']) == tagid:
 	        for j in range(len(json.loads(r0.get(vid))['batchTag'][i]['tagName'])):
@@ -66,5 +62,5 @@ def usr_tags(vid, tagid):
 
 	return usr_tags
 
-def get_tag_cos(TL1,TL2):
+def get_tag_cos(TL1,TL2): #Cosine Similarity Algorithm
     return (len([t for t in TL1 if t in TL2])/math.sqrt(len(TL1)*len(TL2)))
